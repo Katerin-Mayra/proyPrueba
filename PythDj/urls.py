6a6e884fc2ce2e3e82540ead6216a103bba0b822
Katerin  Parra <katerin4468@gmail.com>
2021-01-14 10:26:48 -0400

    firts

--- PythDj/urls.py
+++ PythDj/urls.py
@@ -22,23 +22,34 @@ from principal import views
 
 
 urlpatterns = [
+    #path('url_pagina', views.nombre_funcion, name="dar_nombre"),
     path('admin/', admin.site.urls),
-    path('login/', views.user_login, name="login"), #path('url_pagina', views.nombre_funcion, name="dar_nombre")
+    path('login/', views.user_login, name="login"),                     
     path('check-user/', views.user_login, name="verificar"),            #verificar usuario
     path('login/', views.logout, name="log_out"),                        #deslogear
-    path('inicio/<str:id>/', views.inicio_user, name="inicio"),                    #inicio de usuarios
-    path('list-profiles/<str:id>/', views.list_profiles, name="perfiles"),
+    path('inicio/', views.inicio_user, name="inicio"),                    #inicio de usuarios
     
-    path('create-user/<str:id>/', views.register_user, name="crear_user"),                 #formulario crear usuarios
-    path('create-perfil/<str:id>/', views.register_profile, name="crear_perfil_admin"),    #formulario crear perfil
-    
-    path('delete-user/<str:id>/<str:id_user>/', views.delete_user, name="borrar_user"),                      #borrar usuario
-    path('delete-profile/<str:id>/<str:id_profile>/', views.delete_profile, name="borrar_profile"),          #borrar perfil
+    #path('list-users/', views.list_users, name="usuarios"),                         #listar usuarios
+    path('list-users/', views.UserList.as_view(), name="usuarios"),                 #listar usuarios con clases
+    #path('list-profiles/', views.list_profiles, name="perfiles"),                   #listar perfiles
+    path('list-profiles/', views.ProfileList.as_view(), name="perfiles"),            #listar perfiles con clase
 
-    path('update-profile/<str:id>/', views.create_profile, name="crear_perfil"),                             #actualizar perfil usuario
+    #path('create-user/', views.register_user, name="crear_user"),                 #formulario crear usuarios
+    path('create-user/', views.UserCreate.as_view(), name="crear_user"),                 #formulario crear usuarios con clase
+    #path('create-perfil/', views.register_profile, name="crear_perfil_admin"),    #formulario crear perfil
+    path('create-perfil/', views.ProfileCreate.as_view(), name="crear_perfil_admin"),    #formulario crear perfil con clase    
 
-    path('edit-user/<str:id>/<str:id_user>/', views.edit_user, name="editar_user"),                     #editar usuario
-    path('edit-profile/<str:id>/<str:id_profile>/', views.edit_profile, name="editar_profile"),         #editar perfil
+    #path('delete-user/<str:id_user>/', views.delete_user, name="borrar_user"),                      #borrar usuario
+    path('delete-user/<pk>/', views.UserDelete.as_view(), name="borrar_user"),                      #borrar usuario con clase
+    #path('delete-profile/<str:id_profile>/', views.delete_profile, name="borrar_profile"),          #borrar perfil
+    path('delete-profile/<pk>/', views.ProfileDelete.as_view(), name="borrar_profile"),          #borrar perfil con clase
+
+    path('update-profile/<str:id_user>/', views.create_profile, name="crear_perfil"),                             #actualizar perfil usuario
+
+    #path('edit-user/<str:id_user>/', views.edit_user, name="editar_user"),                     #editar usuario
+    path('edit-user/<pk>', views.UserUpdate.as_view(), name="editar_user"),                     #editar usuario con clase
+    #path('edit-profile/<str:id_profile>/', views.edit_profile, name="editar_profile"),         #editar perfil
+    path('edit-profile/<pk>/', views.ProfileUpdate.as_view(), name="editar_profile"),         #editar perfil
     
 ]
 
--- principal/views.py
+++ principal/views.py
@@ -14,7 +14,8 @@ from django.contrib.auth.forms import UserCreationForm
 from principal.forms import UserForm
 
 #importando clases para CRUD
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 
@@ -31,7 +32,7 @@ def user_login(request):
         if user is not None:
             
             login(request, user)
-            return redirect('inicio', id=user.username)
+            return redirect('inicio')
 
         else:
             return HttpResponse('Usuario no Valido')
@@ -45,78 +46,67 @@ def log_out(request):
     return redirect('login')
 
 #inicio
-def inicio_user(request, id):
-    
-    user = User.objects.get(username=id)
+def inicio_user(request):
+    if request.user.is_superuser:
+        return render(request, 'user_inicio.html')  
+    return render(request, 'user_inicio2.html')  
 
-    if user.is_superuser:
+#lista de usuarios para admin
+def list_users(request):
+    #return HttpResponse('lista de usuarios')
+    users = User.objects.all()
 
-        profiles = Profile.objects.all()
-        users = User.objects.all()
+    return render(request, 'admin_users.html', {
+        'users': users,
+    })
 
-        return render(request, 'admin_inicio.html', {
-            'user': id,
-            'profiles': profiles,
-            'users': users,
-            'nombre_usuario': id,
-        })
-
-    else:
-
-        return render(request, 'user_inicio.html', {
-            'user': "Usuario",
-            'nombre_usuario': id,
-        })
 #lista de perfiles para admin
-def list_profiles(request, id):
+def list_profiles(request):
+    
     profiles = Profile.objects.all()
-    #return HttpResponse('perfiles')
+
     return render(request, 'admin_profiles.html', {
-        'user': id,
         'profiles': profiles,
-        'nombre_usuario': id,
     })
 
 #registro de usuarios
-def register_user(request, id):
+def register_user(request):
     
     if request.method == 'POST':
         
         user_form = UserForm(request.POST)
         if user_form.is_valid():
             user_form.save()
-            return redirect('inicio', id=id)
+            return redirect('inicio')
         else:
             return HttpResponse('error registro')
 
     else:
         form = UserForm()
         return render(request, 'registro_user.html', {
-            'nombre_usuario': id,
             'form': form,
             'tittle': 'Usuario'
         })
 #registro de perfiles
-def register_profile(request, id):
+def register_profile(request):
     
     if request.method == 'POST':
 
         profile_form = FormPerfil(request.POST)
         if profile_form.is_valid():
             profile_form.save()
-            return redirect('perfiles', id=id)
+            return redirect('perfiles')
         else:
             return HttpResponse('error registro')
 
     else:
         form = FormPerfil()
         return render(request, 'registro_profile.html', {
-            'nombre_usuario': id,
             'form': form,
             'tittle': 'Perfil',
         })
 #vista para editar usuarios
-def edit_user(request, id, id_user):
+def edit_user(request, id_user):
     user = User.objects.get(pk=id_user)
     if request.method == "GET":
         
@@ -129,16 +119,15 @@ def edit_user(request, id, id_user):
         if form.is_valid():
             form.save()
 
-        return redirect('inicio', id=id)
+        return redirect('inicio')
     
     return render(request, 'editar_user.html', {
-        'nombre_usuario': id,
         'form': form,
         'id_user': id_user,
     })
 
 #vista para editar perfiles
-def edit_profile(request, id, id_profile):
+def edit_profile(request, id_profile):
     
     perfil = Profile.objects.get(pk=id_profile)
     if request.method == "GET":
@@ -154,35 +143,35 @@ def edit_profile(request, id, id_profile):
         if form.is_valid():
             form.save()
 
-        return redirect('perfiles', id=id)
+        return redirect('perfiles')
     
     #return HttpResponse('hasta aqui todo bien')
     return render(request, 'editar_profile.html', {
-        'nombre_usuario': id,
         'form': form,
         'id_profile': id_profile,
     })
 
 #vista para borrar usuarios
-def delete_user(request, id, id_user):
+def delete_user(request, id_user):
 
     user = User.objects.get(pk=id_user)
     user.delete()
-    return redirect('inicio', id=id)
+    return redirect('inicio')
 
 #vista para borrar perfiles
-def delete_profile(request, id, id_profile):
+def delete_profile(request, id_profile):
     
     perfil = Profile.objects.get(pk=id_profile)
     perfil.delete()
     
-    return redirect('perfiles', id=id)
+    return redirect('perfiles')
 
 #subir perfil desde usuarios
-def create_profile(request, id):
+def create_profile(request, id_user):
     
-    user = User.objects.get(username=id)
-    
+    user = User.objects.get(username=id_user)
+    #return HttpResponse(user.email)
+
     if request.method == 'POST':
         
         form = FormPerfil(request.POST)
@@ -197,24 +186,69 @@ def create_profile(request, id):
         else:
             return HttpResponse('error de formulario')
         
-        if user.is_superuser:
-            return redirect('inicio', id=id)
-        else:
-            return redirect('user', id=user.username)
+        return redirect('inicio')
 
     else:
-
         try:
             perfil = Profile.objects.get(email = user.email)
+            
             return render(request, 'profile.html', {
                 'profile': perfil,
-                'nombre_usuario': id,
             })
         except:
+            
             formulario = FormPerfil()
-
+            
             return render(request, 'user_profile.html', {
                     'form': formulario,
-                    'nombre_usuario': id
             })
 
+#listar usuarios con clase
+class UserList(ListView):
+    model = User
+    template_name = 'admin_users.html'
+
+#listar perfiles conm clase :v
+class ProfileList(ListView):
+    model = Profile
+    template_name = 'admin_profiles.html'
+
+#registrar usuarios con clase
+class UserCreate(CreateView):
+    model = User
+    form_class = UserForm
+    template_name = 'registro_user.html'
+    success_url = reverse_lazy('usuarios')
+
+#registrar perfiles con clase :v
+class ProfileCreate(CreateView):
+    model = Profile
+    form_class = FormPerfil
+    template_name = 'registro_profile.html'
+    success_url = reverse_lazy('perfiles')
+
+#actualizar usuarios
+class UserUpdate(UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'editar_user.html'
+    success_url = reverse_lazy('usuarios')
+
+#actualizar perfiles
+class ProfileUpdate(UpdateView):
+    model = Profile
+    form_class = FormPerfil
+    template_name = 'editar_profile.html'
+    success_url = reverse_lazy('perfiles')
+
+#borrar usuarios
+class UserDelete(DeleteView):
+    model = User
+    template_name = 'delete_user.html'
+    success_url = reverse_lazy('usuarios')
+
+#borrar perfiles
+class ProfileDelete(DeleteView):
+    model = Profile
+    template_name = 'delete_profile.html'
+    success_url = reverse_lazy('perfiles')
